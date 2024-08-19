c3479561505c760f11f487a93c56a5c4001ed4f7
Holguer Pinzon <93726097+Hiceklent@users.noreply.github.com>
2024-08-19 13:54:16 -0500

    [UPDATE] Url Images

--- api/server.js
+++ api/server.js
@@ -1,17 +1,7 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 
 const server = jsonServer.create()
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
-
-// Comment out to allow write operations
 const router = jsonServer.router('data.json')
 
 const middlewares = jsonServer.defaults()
@@ -22,19 +12,19 @@ server.use(middlewares)
 server.get('/api', (req, res) => {
     const baseUrl = req.protocol + '://' + req.get('host') + '/api';
     const endpoints = {
-      destinations: `${baseUrl}/destinations`,
-      crew: `${baseUrl}/crew`,
-      technology: `${baseUrl}/technology`
+        destinations: `${baseUrl}/destinations`,
+        crew: `${baseUrl}/crew`,
+        technology: `${baseUrl}/technology`
     };
     res.json(endpoints);
-  });
-  
-  // Rewrite the API routes
-  server.use(jsonServer.rewriter({
+});
+
+
+server.use(jsonServer.rewriter({
     '/api/destinations': '/destinations',
     '/api/crew': '/crew',
     '/api/technology': '/technology'
-  }));
+}));
 
 
 server.use(router)
